dc33d38f872a0824ce2f50a5c23d32767846c90f
CashewRose <danieagnoletti@gmail.com>
2018-06-18 15:56:42 -0500

    Added an ally toaster that triggers if you join up with them.

--- src/Story/Welcome.js
+++ src/Story/Welcome.js
@@ -27,31 +27,31 @@ class Welcome extends Component {
     if(newAmount === null){
       const damage = "0"
       this.setState({damage})
-      this.SnackBar()
+      this.SnackBar("snackbar")
     }
     else if (newAmount <= 0) {
       const damage = this.state.player.currentHealth - newAmount
       this.setState({player: {...this.state.player, currentHealth: 0}})
       this.setState({damage})
-      this.SnackBar()
+      this.SnackBar("snackbar")
     }
     else {
       const damage = this.state.player.currentHealth - newAmount
       this.setState({player: {...this.state.player, currentHealth: newAmount}})
       this.setState({damage})
-      this.SnackBar()
+      this.SnackBar("snackbar")
     }
   }.bind(this)
 
-  SnackBar = function() {
+  SnackBar = function(id) {
     // Get the snackbar DIV
-    var x = document.getElementById("snackbar");
+    var x = document.getElementById(id);
 
     // Add the "show" class to DIV
     x.className = "show";
 
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4000);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
   Allystats = function() {
@@ -67,13 +67,16 @@ class Welcome extends Component {
     
   // Makes ally active
   AllyActive = function() {
-    this.setState({player: {...this.state.player, allyActive: true}})
+    this.setState({player: {...this.state.player, allyActive: true}}, () => {
+      this.SnackBar("snackbarAlly");
+    })
   }.bind(this)
 
   render() {
     return (
       <div className="App">
         <div id="snackbar">You have taken {this.state.damage} damage</div>
+        <div id="snackbarAlly">You have gained {this.state.ally.name} as an ally!</div>
         < Stats player={this.state.player} ally={this.state.ally}/>
         < Story activate={this.AllyActive} AdjustHealth={this.AdjustHealth} player={this.state.player} props={this.props.history}/>
       </div>
